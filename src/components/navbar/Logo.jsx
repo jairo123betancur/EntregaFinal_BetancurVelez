67415b19a3bd765e6862875521c80e0c6cf71045
jairo123betancur <jabetancurv@hotmail.com>
2025-03-13 14:51:16 -0500

    Ajustando logo

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,7 +1,9 @@
+import './Logo.css';
+
 const Logo = () => {
   return (
     <h1 className="logo">
-      <img src="../../andrew-dev.svg" alt="Logo de la tienda" />
+      <img src="/andrew-dev.svg" alt="Logo de la tienda" />
     </h1>
   );
 };
